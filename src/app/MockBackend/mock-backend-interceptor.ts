95dcffc6c4a79905564b2128e413929ce3fc0284
Daniel Payet <danieldu974@gmail.com>
2018-11-20 16:34:25 +0100

    mock du serveur pour pas attendre

---
+++ src/app/MockBackend/mock-backend-interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { ApiRest, RestApi } from './mock-api';
+
+export function mockBackEndInterceptorFactory() {
+    if (environment.isServeurMock) {
+        return new MockBackendInterceptor();
+    } else {
+        return new EmptyBackendInterceptor();
+    }
+}
+
+@Injectable()
+class MockBackendInterceptor implements HttpInterceptor {
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const api: RestApi = ApiRest.find(apiRest => request.url.endsWith(apiRest.url) && request.method === apiRest.method);
+        if (api) {
+            return of(new HttpResponse({ status: 200, body: api.objectToReturn }));
+        }
+        return next.handle(request);
+    }
+}
+
+@Injectable()
+class EmptyBackendInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request);
+    }
+}
