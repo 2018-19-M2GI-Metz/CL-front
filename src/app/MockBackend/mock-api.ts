5785311590eb5e9db81e902290834da7157b4024
Thibert Letullier <thibert57@me.com>
2018-11-26 10:02:16 +0100

    Add 3 mock method to the API

--- src/app/MockBackend/mock-api.ts
+++ src/app/MockBackend/mock-api.ts
@@ -1,10 +1,28 @@
-const position = { x: 1, y: 1 };
+import { Position } from '../Model/position';
+import { Path } from '../Model/path';
+
+const position1: Position = { id: 1, lon: 1.123, lat: 3.321 };
+const position2: Position = { id: 2, lon: 32.123, lat: 40.321 };
+const position3: Position = { id: 3, lon: 48.123, lat: 64.321 };
+const path1: Path = { start: position1, end: position2 };
+const path2: Path = { start: position2, end: position3 };
+const path3: Path = { start: position3, end: position1 };
 
 export const ApiRest: RestApi[] = [
     {
-        url: "/api/position",
+        url: "/api/nearestpoint",
         method: "GET",
-        objectToReturn: position
+        objectToReturn: position1
+    },
+    {
+        url: "/api/path",
+        method: "GET",
+        objectToReturn: [path1, path2]
+    },
+    {
+        url: "/api/tsp",
+        method: "GET",
+        objectToReturn: [path1, path2, path3]
     }
 ];
 
