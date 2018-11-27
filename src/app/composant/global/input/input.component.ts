f754fb259a242d667d69259794ec39a9e926f572
Daniel Payet <danieldu974@gmail.com>
2018-11-27 19:32:34 +0100

    forumulaire des villes pour gps

--- src/app/composant/global/input/input.component.ts
+++ src/app/composant/global/input/input.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'cl-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, ControlValueAccessor {
+  @Input() placeholder: string;
+  @Input() disabled = false;
+  @Input() _value: string;
+  private propagateChange = (_: any) => { };
+
+  get value(): string {
+    return this._value;
+  }
+
+  set value(val: string) {
+    this._value = val;
+    this.propagateChange(this._value);
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  writeValue(val: string) {
+    this._value = val;
+    this.propagateChange(this._value);
+  }
+
+  registerOnChange(fn: (val: string) => void) {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched() { }
 
 }
--- src/app/composant/main/panel/gps/gps.component.ts
+++ src/app/composant/main/panel/gps/gps.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'cl-gps',
@@ -6,11 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gps.component.scss']
 })
 export class GpsComponent implements OnInit {
+  public gpsForm: FormGroup;
+  public villesSupp: string[] = [];
+  public inputValidators: ValidatorFn[];
 
-  constructor() { }
-
-  ngOnInit() {
-    console.log("helo");
+  constructor() {
+    this.inputValidators = [Validators.required, Validators.minLength(1)];
+    this.gpsForm = new FormGroup({
+      villeDepart: new FormControl(undefined, this.inputValidators),
+      villeArrivee: new FormControl(undefined, this.inputValidators)
+    });
   }
 
+  ngOnInit() {
+  }
+
+  public onSubmit() {
+
+  }
+
+  addCity() {
+    const formControlName = `city-${this.villesSupp.length}`;
+    this.villesSupp.push(formControlName);
+    this.gpsForm.addControl(formControlName, new FormControl(undefined, this.inputValidators));
+  }
+
+  removeCitySupp(event) {
+    event.preventDefault();
+    const cityControlName = this.villesSupp[this.villesSupp.length - 1];
+    this.villesSupp = this.villesSupp.filter(v => v !== cityControlName);
+    this.gpsForm.removeControl(cityControlName);
+  }
 }
