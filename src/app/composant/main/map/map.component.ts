1fe4bd4e5292a50b10bc229b1b7ee03f6d3d3ca4
Daniel Payet <danieldu974@gmail.com>
2018-12-07 00:18:20 +0100

    show user location with icon

--- src/app/composant/main/map/map.component.ts
+++ src/app/composant/main/map/map.component.ts
@@ -3,6 +3,7 @@ import * as mappaMundi from 'mappa-mundi';
 import { HttpService } from 'services/http-service.service';
 import { Position } from 'model/position';
 import { UserLocationService } from 'services/user-location.service';
+import { PointerIcons } from './icones/icones';
 
 @Component({
   selector: 'cl-map',
@@ -14,6 +15,8 @@ export class MapComponent implements OnInit, AfterViewInit {
   private map;
   private ctx;
   private positions: Position[] = [];
+  private userPosition: Position;
+  private userPointer;
 
   constructor(private httpService: HttpService, private userLocationService: UserLocationService) { }
 
@@ -23,12 +26,24 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   async ngAfterViewInit() {
     this.initPositions();
+    await this.initUserLocation();
     await this.initMap();
     this.drawMap();
   }
 
   private async initPositions() {
     this.positions.push(...await this.httpService.getAllPositions());
+  }
+
+  private initUserLocation() {
+    return new Promise(async (res) => {
+      this.userPosition = await this.userLocationService.getUserLocation();
+      this.userPointer = new Image();
+      this.userPointer.src = PointerIcons;
+      this.userPointer.onload = () => {
+        res();
+      }
+    });
   }
 
   private initMap(): Promise<{}> {
@@ -38,11 +53,10 @@ export class MapComponent implements OnInit, AfterViewInit {
       this.ctx = this.canvas.nativeElement.getContext("2d");
       this.canvas.nativeElement.width = window.innerWidth;
       this.canvas.nativeElement.height = window.innerHeight;
-      const userPosition: Position = await this.userLocationService.getUserLocation();
       const options = {
-        lat: userPosition ? userPosition.lat : 0,
-        lng: userPosition ? userPosition.lon : 0,
-        zoom: userPosition ? 10 : 3,
+        lat: this.userPosition ? this.userPosition.lat : 0,
+        lng: this.userPosition ? this.userPosition.lon : 0,
+        zoom: this.userPosition ? 10 : 3,
         // style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
       };
       this.map = mappa.tileMap(options);
@@ -55,6 +69,7 @@ export class MapComponent implements OnInit, AfterViewInit {
     if (this.map) {
       this.map.onChange(() => {
         this.clearMap();
+        this.drawUserLocation();
         this.drawPositions();
       });
     }
@@ -64,10 +79,14 @@ export class MapComponent implements OnInit, AfterViewInit {
     this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   }
 
+  private drawUserLocation(){
+    const positionPixels = this.map.latLngToPixel(this.userPosition.lat, this.userPosition.lon);
+    this.ctx.drawImage(this.userPointer, positionPixels.x, positionPixels.y);
+  }
+
   private drawPositions() {
     this.positions.forEach(position => {
       const positionPixels = this.map.latLngToPixel(position.lon, position.lat);
-
       this.ctx.beginPath();
       this.ctx.lineWidth = "6";
       this.ctx.strokeStyle = "red";
