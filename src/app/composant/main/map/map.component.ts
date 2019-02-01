a6b38dd6ebd11c5aa782f14693edc03505b84789
Daniel Payet <danieldu974@gmail.com>
2019-02-01 08:20:46 +0100

    resize fonctionne 1 fois sur 5

--- src/app/composant/main/map/map.component.ts
+++ src/app/composant/main/map/map.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import * as mappaMundi from 'mappa-mundi';
 import { Place } from 'model/place';
 import { UserLocationService } from 'services/user-location.service';
 import { DrawerService } from 'services/drawer.service';
 import { MapDataService } from 'services/map-data.service';
+import { Subscription } from 'rxjs';
+
+const KEY = 'pk.eyJ1IjoiZGFuaWVscGF5ZXQiLCJhIjoiY2pvem1leGF0Mm1hOTN3cGhmbHM0b3p2ayJ9.s0Gdr8eabQi56tHONKv1Sg';
 
 @Component({
   selector: 'cl-map',
@@ -12,18 +15,48 @@ import { MapDataService } from 'services/map-data.service';
 })
 export class MapComponent implements OnInit {
   @ViewChild("map") canvas: ElementRef;
+  @ViewChild("container") container: ElementRef;
   private map;
   private positions: Place[] = [];
+  private canvasTmp: ElementRef;
+  private mapDataOnChange: Subscription;
+  private zoomState: number = undefined;
+  private positionMapState: Place = undefined;
+  private mappa;
 
   constructor(
     private userLocationService: UserLocationService,
     private drawerService: DrawerService,
-    private mapData: MapDataService) { }
+    private mapData: MapDataService,
+    private renderer: Renderer2) {
+    this.mappa = new mappaMundi('Mapbox', KEY);
+  }
 
   async ngOnInit() {
+    this.canvasTmp = this.canvas;
     this.initPositions();
     await this.initMap();
     this.drawMap();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (this.map) {
+      this.zoomState = this.map.zoom();
+      const position = this.map.fromPointToLatLng(Math.floor(window.innerWidth / 2), Math.floor(window.innerHeight / 2));
+      this.positionMapState = new Place(undefined, "curent_Position", position.lat, position.lng);
+      this.mapDataOnChange.unsubscribe();
+      this.map.onChange(() => { });
+    }
+    for (const child of this.container.nativeElement.children) {
+      this.renderer.removeChild(this.container.nativeElement, child);
+    }
+    this.canvas = this.canvasTmp;
+    try {
+      this.renderer.removeChild(document.getElementsByTagName("body").item(0), document.getElementById("Mapbox"));
+    } catch (e) { }
+    this.renderer.appendChild(this.container.nativeElement, this.canvas.nativeElement);
+    this.ngOnInit();
   }
 
   private async initPositions() {
@@ -32,22 +65,29 @@ export class MapComponent implements OnInit {
 
   private initMap(): Promise<{}> {
     return new Promise(async (res) => {
-      const key = 'pk.eyJ1IjoiZGFuaWVscGF5ZXQiLCJhIjoiY2pvem1leGF0Mm1hOTN3cGhmbHM0b3p2ayJ9.s0Gdr8eabQi56tHONKv1Sg';
-      const mappa = new mappaMundi('Mapbox', key);
       this.drawerService.setContext(this.canvas.nativeElement.getContext("2d"));
       this.canvas.nativeElement.width = window.innerWidth;
       this.canvas.nativeElement.height = window.innerHeight;
       const userPosition = await this.userLocationService.getUserLocation();
+      const realPosition = this.getPosition(userPosition);
       const options = {
-        lat: userPosition ? userPosition.lat : 46.483440,
-        lng: userPosition ? userPosition.lon : 2.525914,
-        zoom: userPosition ? 10 : 6,
+        lat: realPosition ? realPosition.lat : 46.483440,
+        lng: realPosition ? realPosition.lon : 2.525914,
+        zoom: this.getZoomState(userPosition),
         // style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
       };
-      this.map = mappa.tileMap(options);
+      this.map = this.mappa.tileMap(options);
       this.map.overlay(this.canvas.nativeElement);
       res();
     });
+  }
+
+  private getPosition(userPosition): Place {
+    return this.positionMapState || userPosition;
+  }
+
+  private getZoomState(userPosition): number {
+    return this.zoomState || (userPosition ? 10 : 6);
   }
 
   private drawMap() {
@@ -60,7 +100,7 @@ export class MapComponent implements OnInit {
     };
     if (this.map) {
       this.map.onChange(updateDraw);
-      this.mapData.onChange().subscribe(updateDraw);
+      this.mapDataOnChange = this.mapData.onChange().subscribe(updateDraw);
     }
   }
 
